0ad67a905cb7ce1e03257fd9b5d2d5c7dd1afa1e
Ngô Văn Quân <25648131.quan@student.iuh.edu.vn>
2026-01-02 21:00:12 +0700

    change

--- styles.js
+++ styles.js
@@ -2,11 +2,19 @@ function inputForm() {
     let name = document.getElementById("name").value;
     let mssv = document.getElementById("mssv").value;
     if (name === "" || mssv === "") {
-        document.getElementById("demo").style.display = "block";
+        document.getElementById(
+            "demo"
+        ).innerText = `Vui lòng nhập đầy đủ thông tin!`;
+        return;
+    }
+    if (!/^[0-9]{8}$/.test(mssv)) {
+        document.getElementById(
+            "demo"
+        ).innerText = `MSSV chỉ được phép ghi số!`;
         return;
     }
     document.getElementById("containerForm").style.display = "none";
     document.getElementById("formOutput").style.display = "flex";
-    document.getElementById("nameOutput").innerText = `Họ và tên : ${name}`;
-    document.getElementById("mssvOutput").innerText = `MSSV : ${mssv}`;
+    document.getElementById("nameOutput").innerText = name;
+    document.getElementById("mssvOutput").innerText = mssv;
 }
